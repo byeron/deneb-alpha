431a17434b7b240589cff549d546ebe9f9c43fe9
byeron <disce.uno@gmail.com>
2023-10-19 10:24:54 +0900

    delete: unused dependency

--- domain/ftest_config.py
+++ domain/ftest_config.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from domain.interface.fluctuation_config import IFluctuationConfig
-from domain.interface.multipletest_config import IMultipletestConfig
 
 
 @dataclass(frozen=True)
