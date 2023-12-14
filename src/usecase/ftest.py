5754cc507d9e854313a8bd6500bb1c8319684704
byeron <disce.uno@gmail.com>
2023-12-14 14:13:50 +0900

    add: FtestConfigで指定されたindexのラベルが存在しない場合にエラーを出す
    <body: why>
    
    ---
    add: (機能・ファイルを)追加する
    fix: (コードを)修正する
    clean: (コードを)改善する; 名前の変更/コードの整形
    update: (パッケージやドキュメントなどを更新する)
    remove: (ファイルやコードを)除去する
    rename: (ファイル名を)変更する
    change: (AをBに)変更する
    
    ref: https://www.tam-tam.co.jp/tipsnote/program/post16686.html

--- src/usecase/ftest.py
+++ src/usecase/ftest.py
@@ -24,6 +24,11 @@ class Ftest(Fluctuation):
 
     def run(self, feature_data: FeatureData):
         df = feature_data.matrix
+        # Check if the control and experiment index exist
+        if self.control not in df.index:
+            raise ValueError(f"{self.control} is not in the index")
+        if self.experiment not in df.index:
+            raise ValueError(f"{self.experiment} is not in the index")
 
         control = df.loc[self.control, :]
         experiment = df.loc[self.experiment, :]
@@ -31,6 +36,13 @@ class Ftest(Fluctuation):
         # Calculate F-value
         control_var = control.var(ddof=1, axis=0)
         experiment_var = experiment.var(ddof=1, axis=0)
+
+        # Control群の各カラムに分散が0のものが含まれている場合、0除算によるエラーをthrowする
+        if control_var.isin([0]).any():
+            # 分散が0の要素名を取得
+            zero_var_columns = control_var[control_var == 0].index.tolist()
+            raise ValueError(f"Control group has zero variance columns: {zero_var_columns}")
+
         f_value = experiment_var / control_var
         p_value = f.sf(f_value, dfn=(len(experiment) - 1), dfd=(len(control) - 1))
         reject = [p_value < self.alpha for p_value in p_value]
