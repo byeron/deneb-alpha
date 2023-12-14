df9c785b84cd092fcdfbf65eababd168e9d95113
byeron <disce.uno@gmail.com>
2023-12-14 14:16:50 +0900

    add: 存在しない実験群を指定したときにエラーが送出できる
    <body: why>
    
    ---
    add: (機能・ファイルを)追加する
    fix: (コードを)修正する
    clean: (コードを)改善する; 名前の変更/コードの整形
    update: (パッケージやドキュメントなどを更新する)
    remove: (ファイルやコードを)除去する
    rename: (ファイル名を)変更する
    change: (AをBに)変更する
    
    ref: https://www.tam-tam.co.jp/tipsnote/program/post16686.html

--- src/usecase/abslinear.py
+++ src/usecase/abslinear.py
@@ -9,6 +9,11 @@ class AbsLinear(Dissimilarity):
         f = feature_data.fluctuation
         if f.empty:
             raise ValueError('The fluctuating variable does not exist.')
+
+        # 指定したexperimentがindexに存在しない場合、エラーを返す
+        if self.config.experiment not in f.index:
+            raise ValueError(f"The experiment '{self.config.experiment}' does not exist.")
+
         f = f.loc[self.config.experiment, :]
 
         dissimilarity = 1 - abs(f.corr(method=self.config.corr_method))
