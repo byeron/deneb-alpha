d10223bbe28d2e0fb648d7cbed09274b9284c078
byeron <me@lab>
2024-06-17 14:17:09 +0900

    add: 中央絶対偏差を用いたゆらぎの評価を行う機能を追加した ---
    <body: why>
    
    ---
    add: (機能・ファイルを)追加する
    
    fix: (コードを)修正する
    
    clean: (コードを)改善する; 名前の変更/コードの整形
    
    update: (パッケージやドキュメントを)更新する
    
    remove: (ファイルやコードを)取り除く
    
    rename: (ファイル名を)変更する
    
    change: (AをBに)変更する

--- src/factory/fluctuation.py
+++ src/factory/fluctuation.py
@@ -2,27 +2,43 @@ from injector import Module
 
 from domain.ftest_config import FtestConfig
 from domain.interface.fluctuation import IFluctuation
+from domain.mad_ratio_config import MadRatioConfig
 from usecase.ftest import Ftest
+from usecase.mad_ratio import MADRatio
 
 
 class FluctuationFactory(Module):
-    def __init__(self, control, experiment, alpha):
-        self.config = FtestConfig(
-            control=control,
-            experiment=experiment,
-            alpha=alpha,
-        )
+    def __init__(self, control, experiment, method, alpha=None, mad_threshold=None):
+        self.method = method
+        match method:
+            case "ftest":
+                if alpha is None:
+                    raise ValueError("alpha is required for ftest method")
+                self.config = FtestConfig(
+                    control=control,
+                    experiment=experiment,
+                    alpha=alpha,
+                )
+            case "mad-ratio":
+                if mad_threshold is None:
+                    raise ValueError("mad_threshold is required for mad method")
+                self.config = MadRatioConfig(
+                    control=control,
+                    experiment=experiment,
+                    threshold=mad_threshold,
+                )
+                print(self.config)
+            case _:
+                raise ValueError("method is not supported")
 
     def factory(self):
-        # Future plan
-        """
-        match self.config.method:
-            case hoge:
-                return Hoge()
+        match self.method:
+            case "ftest":
+                return Ftest(self.config)
+            case "mad-ratio":
+                return MADRatio(self.config)
             case _:
                 raise ValueError
-        """
-        return Ftest(self.config)
 
     def configure(self, binder):
         binder.bind(IFluctuation, to=self.factory())
---
+++ src/usecase/mad_ratio.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from domain.feature_data import FeatureData
+from domain.interface.fluctuation_config import IFluctuationConfig
+from usecase.fluctuation import Fluctuation
+
+
+class MADRatio(Fluctuation):
+    def __init__(self, config: IFluctuationConfig):
+        super().__init__(config)
+
+        if not hasattr(self.config, "control"):
+            raise AttributeError("control is required")
+        if not hasattr(self.config, "experiment"):
+            raise AttributeError("experiment is required")
+        if not hasattr(self.config, "threshold"):
+            raise AttributeError("mad_threshold is required")
+
+        self.control = self.config.control
+        self.experiment = self.config.experiment
+        self.threshold = self.config.threshold
+
+    def run(self, feature_data: FeatureData):
+        df = feature_data.matrix
+
+        if self.control not in df.index:
+            raise ValueError(f"control {self.control} is not in the index")
+        if self.experiment not in df.index:
+            raise ValueError(f"experiment {self.experiment} is not in the index")
+
+        ctrl = df.loc[self.control, :]
+        expr = df.loc[self.experiment, :]
+
+        # 各列ごとの中央絶対偏差を計算
+        # controlのMAD
+        mad_ctrl = np.median(np.abs(ctrl - ctrl.median(axis=0)), axis=0)
+        mad_expr = np.median(np.abs(expr - expr.median(axis=0)), axis=0)
+
+        # 実験群のMADが対象群のMADの2倍以上の場合、True, それ以外はFalse
+        diff = mad_expr - mad_ctrl * self.threshold
+        reject = diff > 0
+
+        return (diff, reject)
